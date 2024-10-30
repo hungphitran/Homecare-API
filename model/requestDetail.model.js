91405fbf05c4e40299a654ea52b93373e1becc65
hungphitran <hungphitran.22@gmail.com>
2024-10-30 17:04:48 +0700

    edit requestDetail model

--- model/requestDetail.model.js
+++ model/requestDetail.model.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const requestDetailSchema = new mongoose.Schema({
     workingDate: Date,
+    startTime: String, // Giờ bắt đầu làm việc
+    endTime: String, // Giờ kết thúc làm việc
     helper_id: String,
     status: String, // notDone - processing - done
     helper_cost: Number
