b825486f42c1dcd3c419f3340d9364aca9b582c6
hungphitran <hungphitran.22@gmail.com>
2024-11-23 10:28:48 +0700

    fixed requestController

--- controller/requestController.js
+++ controller/requestController.js
@@ -4,15 +4,16 @@ const RequestDetail= require('../model/requestDetail.model')
 const requestController ={
     //POST a new request
     create: async (req,res,next)=>{
-
-        req.body.customerInfo = req.body.customerInfo;
+        console.log(typeof req.body.customerInfo)
+        if( typeof req.body.customerInfo == "string"){
+            req.body.customerInfo = JSON.parse(req.body.customerInfo);
+        }
         req.body.customerInfo.usedpoint=0;
         req.body.orderDate =new Date(req.body.orderDate)
         let dates =(req.body.startDate).split(',')
         dates=dates.filter((value,index)=>{
             return value;
         })
-
         let scheduleIds= []
         for(let workingDate of dates){
             let reqDetail= new RequestDetail({
