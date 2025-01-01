533964ab4d918802e4b1d0f2eb7949878239f936
hungphitran <hungphitran.22@gmail.com>
2025-01-01 20:27:53 +0700

    lock generalSetting route

--- controller/generalSettingController.js
+++ controller/generalSettingController.js
@@ -1,15 +1,15 @@
-const generalSettingModel = require("../model/generalSetting.model");
+// const GeneralSetting = require("../model/generalSetting.model");
 
-const generalSettingController = {
-    getAll: async (req, res) => {
-        await generalSettingModel.findOne()
-            .then((data) => {
-                res.status(200).json(data);
-            })
-            .catch((err) => {
-                res.status(500).json(err);
-            });
-    },
-}
+// const generalSettingController = {
+//     getAll: async (req, res) => {
+//         await GeneralSetting.findOne()
+//             .then((data) => {
+//                 res.status(200).json(data);
+//             })
+//             .catch((err) => {
+//                 res.status(500).json(err);
+//             });
+//     },
+// }
 
-module.exports = generalSettingController;
+// module.exports = generalSettingController;
