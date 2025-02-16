7d671fe3a55b344886b596bc537e8ab18111655b
hungphitran <hungphitran.22@gmail.com>
2025-02-16 19:12:03 +0700

    add handle for invalid date

--- controller/requestController.js
+++ controller/requestController.js
@@ -4,9 +4,22 @@ const RequestDetail= require('../model/requestDetail.model')
 const requestController ={
     //POST a new request
     create: async (req,res,next)=>{
+        console.log(req.body)
         if( typeof req.body.customerInfo == "string"){
             req.body.customerInfo = JSON.parse(req.body.customerInfo);
         }
+            let orderDate = req.body.orderDate;
+            let tmp=orderDate.split('-')
+            let month = tmp[1];
+            if(month.length<2){
+                month="0"+month;
+            }
+            let day = tmp[2];  
+            if(day.length<2){
+                day="0"+day;
+            }
+            req.body.orderDate = tmp[0]+"-"+month+"-"+day;
+
         req.body.customerInfo.usedpoint=0;
 
         req.body.orderDate =new Date(req.body.orderDate)
@@ -47,9 +60,9 @@ const requestController ={
             totalCost:req.body.totalCost,
             status:"Chưa tiến hành"
         })
-
+        res.send(newOrder)
         await newOrder.save()
-        //.then(()=>res.status(200).json("success"))
+        .then(()=>res.status(200).json("success"))
         .catch((err)=> res.status(500).json(err))
     },
     // GET all request in database
